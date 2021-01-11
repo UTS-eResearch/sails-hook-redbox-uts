87551478c04b9d184eecc02a567175e373254a46
Moises Sacal <moisbo@gmail.com>
2021-01-11 14:15:45 +1100

    updated form location folder

--- config/form.js
+++ config/form.js
@@ -2,14 +2,14 @@
  * Form related configuration
  */
 var _ = require('lodash');
-var dataRecordForm = require('../form-config-new/dataRecord-1.0-draft.js');
-var rdmpForm = require('../form-config-new/default-1.0-draft.js');
-var dataPublicationForm = _.cloneDeep(require('../form-config-new/dataPublication-1.0-draft.js'));
-var dataPublicationEmbargoedForm = _.cloneDeep(require('../form-config-new/dataPublication-1.0-embargoed.js'));
-var dataPublicationPublishedForm = _.cloneDeep(require('../form-config-new/dataPublication-1.0-published.js'));
-var dataPublicationRetiredForm = _.cloneDeep(require('../form-config-new/dataPublication-1.0-retired.js'));
-var dataPublicationReviewingForm = _.cloneDeep(require('../form-config-new/dataPublication-1.0-reviewing.js'));
-var genericWorkspaceForm = require('../form-config-new/genericWorkspace-1.0-draft.js');
+var dataRecordForm = require('../form-config/dataRecord-1.0-draft.js');
+var rdmpForm = require('../form-config/default-1.0-draft.js');
+var dataPublicationForm = _.cloneDeep(require('../form-config/dataPublication-1.0-draft.js'));
+var dataPublicationEmbargoedForm = _.cloneDeep(require('../form-config/dataPublication-1.0-embargoed.js'));
+var dataPublicationPublishedForm = _.cloneDeep(require('../form-config/dataPublication-1.0-published.js'));
+var dataPublicationRetiredForm = _.cloneDeep(require('../form-config/dataPublication-1.0-retired.js'));
+var dataPublicationReviewingForm = _.cloneDeep(require('../form-config/dataPublication-1.0-reviewing.js'));
+var genericWorkspaceForm = require('../form-config/genericWorkspace-1.0-draft.js');
 
 module.exports.form = {
   defaultForm: "default-1.0-draft",
